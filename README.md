955dc851f31dca1e07483e8afcd78d12eac87d52
Tomas David <tdavid@redhat.com>
2020-03-05 11:00:18 +0100

    Improve basic project structure

---
+++ README.md
@@ -0,0 +1,1 @@
+# Kogito Google Chrome extension tests
---
+++ test/SimpleTest.ts
@@ -0,0 +1,7 @@
+import { expect } from 'chai';
+
+describe("Simple test", () => {
+    it("should test", () => {
+        expect(true).equal(true);
+    })
+})
