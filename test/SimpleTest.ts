ee942c7432cdfc7816b34fb8ca1f818c3adef78d
Tomas David <tdavid@redhat.com>
2020-03-06 13:23:28 +0100

    Try simple selenium

--- test/SimpleTest.ts
+++ test/SimpleTest.ts
@@ -27,9 +27,9 @@ describe("Simple test", () => {
     });
 
 
-    it("should test", async () => {
-        await driver.get("https://github.com/kiegroup/kie-wb-playground/blob/master/evaluation/src/main/resources/");
-        await driver.findElement(By.linkText("evaluation.bpmn")).click();
+    it("should test", () => {
+         driver.get("https://github.com/kiegroup/kie-wb-playground/blob/master/evaluation/src/main/resources/");
+        /*await driver.findElement(By.linkText("evaluation.bpmn")).click();
 
         let kogitoFrame = await driver.wait(until.elementLocated(By.className("kogito-iframe")), 2000);
         await driver.switchTo().frame(kogitoFrame);
@@ -38,7 +38,7 @@ describe("Simple test", () => {
 
         await driver.wait(until.elementLocated(By.className("fa-eye")), 30000);
 
-        await driver.switchTo().defaultContent();
+        await driver.switchTo().defaultContent();*/
 
     }).timeout(60000);
 
