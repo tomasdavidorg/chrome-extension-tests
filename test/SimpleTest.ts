b3dd35577ce23a1d8a42e77fd8c9e9383c3d3b2b
Tomas David <tdavid@redhat.com>
2020-03-09 12:11:52 +0100

    Recreate original test

--- test/SimpleTest.ts
+++ test/SimpleTest.ts
@@ -3,20 +3,22 @@ import { Options } from "selenium-webdriver/chrome";
 
 describe("Simple test", () => {
 
- /*   let driver: ThenableWebDriver;
+    let driver: ThenableWebDriver;
 
     before(() => {
-        const chromeExtensionPath = process.env.CHROME_EXTENSION_PATH;
+        // get path to unzipped extension
+        const chromeExtensionPath = process.env.UNZIPPED_CHROME_EXTENSION_PATH;
         if (!chromeExtensionPath) {
-            throw new Error("Please set CHROME_EXTENSION_PATH variable to unziped chrome extension directory.");
+            throw new Error("Please set UNZIPPED_CHROME_EXTENSION_PATH variable to unziped Chrome extension directory.");
         }
 
+        // init chrome options
         let chromeOptions = new Options();
         chromeOptions.addArguments("--load-extension=" + chromeExtensionPath)
 
         // initializing chrome driver
         driver = new Builder()
-        //    .setChromeOptions(chromeOptions)
+            .setChromeOptions(chromeOptions)
             .forBrowser(Browser.CHROME)
             .withCapabilities(Capabilities.chrome())
             .build();
@@ -24,39 +26,26 @@ describe("Simple test", () => {
         // maximizing chrome browser
         driver.manage().window().maximize();
     });
-*/
+
 
     it("should test", async () => {
-        const chromeExtensionPath = process.env.CHROME_EXTENSION_PATH;
-        if (!chromeExtensionPath) {
-            throw new Error("Please set CHROME_EXTENSION_PATH variable to unziped chrome extension directory.");
-       }
+        await driver.get("https://github.com/kiegroup/kie-wb-playground/blob/master/evaluation/src/main/resources/");
+        await driver.findElement(By.linkText("evaluation.bpmn")).click();
 
-        let chromeOptions = new Options();
-        chromeOptions.addArguments("--load-extension=" + chromeExtensionPath, "--remote-debugging-port=9515")
-        let driver = await new Builder()
-            .setChromeOptions(chromeOptions)
-            .forBrowser(Browser.CHROME)
-            .withCapabilities(Capabilities.chrome())
-            .build();
-
-            await driver.get("https://github.com/kiegroup/kie-wb-playground/blob/master/evaluation/src/main/resources/");
-            await driver.findElement(By.linkText("evaluation.bpmn")).click();
-
-        let kogitoFrame =  await driver.wait(until.elementLocated(By.className("kogito-iframe")), 2000);
+        let kogitoFrame = await driver.wait(until.elementLocated(By.className("kogito-iframe")), 2000);
         await driver.switchTo().frame(kogitoFrame);
 
-        await  driver.wait(until.elementLocated(By.className("fa-eye")), 30000);
+        await driver.wait(until.elementLocated(By.className("fa-eye")), 30000);
 
         await driver.switchTo().defaultContent();
         await driver.quit();
 
     }).timeout(100000);
-/*
+
     after(() => {
         if (driver != undefined) {
             driver.quit();
         }
     });
-    */
+
 })
