872b56f3b343a092152ec316132a9346c97dc6c9
Tomas David <tdavid@redhat.com>
2020-04-17 14:10:04 +0200

    Fix iframe editor locator

--- test/framework/editor/EditorPage.ts
+++ test/framework/editor/EditorPage.ts
@@ -3,7 +3,7 @@ import Editor from "./Editor";
 import { By, WebElement } from "selenium-webdriver";
 
 export default abstract class EditorPage extends Page {
-    private readonly KOGITO_EDITOR_FRAME_LOCATOR = By.tagName("iframe");
+    private readonly KOGITO_EDITOR_FRAME_LOCATOR = By.xpath("//iframe[contains(@class,'kogito-iframe') or contains(@id,'kogito-iframe')]");
 
     public async getEditor(): Promise<Editor> {
         let editor: WebElement = await this.tools.by(this.KOGITO_EDITOR_FRAME_LOCATOR).withTimeout(2000).present();
