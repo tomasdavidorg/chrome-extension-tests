2ca9ab613afa775a60e64d31ef5c9656bd00da70
Tomas David <tdavid@redhat.com>
2020-04-29 14:36:17 +0200

    Change constants to static members

--- test/framework/github-file-list/GitHubListItem.ts
+++ test/framework/github-file-list/GitHubListItem.ts
@@ -4,21 +4,21 @@ import GitHubEditorPage from "../github-editor/GitHubEditorPage";
 
 export default class GitHubListItem extends PageFragment {
 
-    private readonly LINK_LOCATOR: By = By.xpath(".//a[@class='js-navigation-open ']");
-    private readonly LINK_TO_ONLINE_EDITOR: By = By.xpath(".//a[@title='Open in Online Editor']");
+    private static readonly LINK_LOCATOR: By = By.xpath(".//a[@class='js-navigation-open ']");
+    private static readonly LINK_TO_ONLINE_EDITOR: By = By.xpath(".//a[@title='Open in Online Editor']");
 
     public async load(): Promise<void> {
-        await this.tools.by(this.LINK_TO_ONLINE_EDITOR).withTimeout(5000).present();
+        await this.tools.by(GitHubListItem.LINK_TO_ONLINE_EDITOR).withTimeout(5000).present();
     }
 
     public async open(): Promise<GitHubEditorPage> {
-        const link: WebElement = await this.tools.webElement(this.root).find(this.LINK_LOCATOR);
+        const link: WebElement = await this.tools.webElement(this.root).find(GitHubListItem.LINK_LOCATOR);
         await link.click();
         return this.tools.createPage(GitHubEditorPage);
     }
 
     public async getLinkToOnlineEditor(): Promise<string> {
-        const linkToOnlineEditor: WebElement = await this.tools.webElement(this.root).find(this.LINK_TO_ONLINE_EDITOR);
+        const linkToOnlineEditor: WebElement = await this.tools.webElement(this.root).find(GitHubListItem.LINK_TO_ONLINE_EDITOR);
         return linkToOnlineEditor.getAttribute("href");
     }
 }
