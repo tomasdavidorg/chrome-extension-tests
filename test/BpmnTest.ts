8fe49d952ab4725fe6e4a9453250605aec1ef454
Tomas David <tdavid@redhat.com>
2020-05-29 15:41:07 +0200

    Switch to jest

--- test/BpmnTest.ts
+++ test/BpmnTest.ts
@@ -3,80 +3,78 @@ import GitHubEditorPage from "./framework/github-editor/GitHubEditorPage";
 import GitHubListItem from "./framework/github-file-list/GitHubListItem";
 import GitHubListPage from "./framework/github-file-list/GitHubListPage";
 import Tools from "./utils/Tools";
-import { expect } from "chai";
 
-describe("BPMN test", () => {
+let tools: Tools;
 
-    let tools: Tools;
-
-    beforeEach(async () => {
-        tools = await Tools.init();
-    });
-
-    it("basic operations", async () => {
-        const WEB_PAGE = "https://github.com/kiegroup/kie-wb-playground/blob/master/evaluation/src/main/resources/";
-        const EXPECTED_LINK = "/kiegroup/kie-wb-playground/master/evaluation/src/main/resources/evaluation.bpmn";
-        const FILE_NAME = "evaluation.bpmn";
-        const PROCESS_NODES_NAMES = ["Start", "Self Evaluation", "PM Evaluation", "HR Evaluation", "Parallel", "Parallel", "End Terminate"];
-
-        // check link to online editor in the list
-        const gitHubListPage: GitHubListPage = await tools.openPage(GitHubListPage, WEB_PAGE);
-        const gitHubFile: GitHubListItem = await gitHubListPage.getFile(FILE_NAME);
-        const linkText = await gitHubFile.getLinkToOnlineEditor();
-        expect(linkText).contains(EXPECTED_LINK);
-
-        // open BPMN editor
-        let gitHubEditorPage: GitHubEditorPage = await gitHubFile.open();
-        const bpmnEditor: BpmnEditor = await gitHubEditorPage.getBpmnEditor();
-
-        // drag and drop startEvent to canvas
-        await bpmnEditor.enter();
-        await bpmnEditor.dragAndDropStartEventToCanvas();
-
-        // check process properties
-        const sideBar = await bpmnEditor.getSideBar();
-        const processProps = await sideBar.openProperties();
-        expect(await processProps.getProcessNameFromInput()).equals("Evaluation");
-        
-        // check process nodes in explorer
-        const explorer = await sideBar.openExplorer();
-        expect(await explorer.getNodeNames()).to.have.members(PROCESS_NODES_NAMES.concat("Start"));
-        expect(await explorer.getProcessName()).equals("Evaluation");
-
-        // check task properties
-        await explorer.selectNode("PM Evaluation");
-        const nodeProps = await sideBar.openProperties();
-        expect(await nodeProps.getNameFromTextArea()).equals("PM Evaluation");
-
-        await bpmnEditor.leave();
-
-        // open and check source editor
-        expect(await gitHubEditorPage.isSourceVisible()).false;
-        expect(await gitHubEditorPage.isEditorVisible()).true;
-        await gitHubEditorPage.seeAsSource();
-        expect(await gitHubEditorPage.isSourceVisible()).true;
-        expect(await gitHubEditorPage.isEditorVisible()).false;
-        await gitHubEditorPage.seeAsDiagram();
-        expect(await gitHubEditorPage.isSourceVisible()).false;
-        expect(await gitHubEditorPage.isEditorVisible()).true;
-
-        // check link to online editor from clipboard
-        await gitHubEditorPage.copyLinkToOnlineEditor();
-        const clipboadText = await tools.clipboard().getContent();
-        expect(clipboadText).contains(EXPECTED_LINK);
-
-        // open full screen editor
-        const fullScreenPage = await gitHubEditorPage.fullScreen();
-        await fullScreenPage.getBpmnEditor();
-        await fullScreenPage.scrollToTop();
-        gitHubEditorPage = await fullScreenPage.exitFullscreen();
-
-        // open online editor
-        const onlineEditorPage = await gitHubEditorPage.openOnlineEditor();
-        await onlineEditorPage.getBpmnEditor();
-    });
-
-    afterEach(async () => {
-        await tools.finishTest("BPMN_test");
-    });
+beforeEach(async () => {
+    tools = await Tools.init();
 });
+
+test("BPMN basic operations test", async () => {
+
+    const WEB_PAGE = "https://github.com/kiegroup/kie-wb-playground/blob/master/evaluation/src/main/resources/";
+    const EXPECTED_LINK = "/kiegroup/kie-wb-playground/master/evaluation/src/main/resources/evaluation.bpmn";
+    const FILE_NAME = "evaluation.bpmn";
+    const PROCESS_NODES_NAMES = ["Start", "Self Evaluation", "PM Evaluation", "HR Evaluation", "Parallel", "Parallel", "End Terminate"];
+
+    // check link to online editor in the list
+    const gitHubListPage: GitHubListPage = await tools.openPage(GitHubListPage, WEB_PAGE);
+    const gitHubFile: GitHubListItem = await gitHubListPage.getFile(FILE_NAME);
+    const linkText = await gitHubFile.getLinkToOnlineEditor();
+    expect(linkText).toContain(EXPECTED_LINK);
+
+    // open BPMN editor
+    let gitHubEditorPage: GitHubEditorPage = await gitHubFile.open();
+    const bpmnEditor: BpmnEditor = await gitHubEditorPage.getBpmnEditor();
+
+    // drag and drop startEvent to canvas
+    await bpmnEditor.enter();
+    await bpmnEditor.dragAndDropStartEventToCanvas();
+
+    // check process properties
+    const sideBar = await bpmnEditor.getSideBar();
+    const processProps = await sideBar.openProperties();
+    expect(await processProps.getProcessNameFromInput()).toEqual("Evaluation");
+
+    // check process nodes in explorer
+    const explorer = await sideBar.openExplorer();
+    expect(await (await explorer.getNodeNames()).sort()).toEqual(PROCESS_NODES_NAMES.concat("Start").sort());
+    expect(await explorer.getProcessName()).toEqual("Evaluation");
+
+    // check task properties
+    await explorer.selectNode("PM Evaluation");
+    const nodeProps = await sideBar.openProperties();
+    expect(await nodeProps.getNameFromTextArea()).toEqual("PM Evaluation");
+
+    await bpmnEditor.leave();
+
+    // open and check source editor
+    expect(await gitHubEditorPage.isSourceVisible()).toBe(false);
+    expect(await gitHubEditorPage.isEditorVisible()).toBe(true);
+    await gitHubEditorPage.seeAsSource();
+    expect(await gitHubEditorPage.isSourceVisible()).toBe(true);
+    expect(await gitHubEditorPage.isEditorVisible()).toBe(false);
+    await gitHubEditorPage.seeAsDiagram();
+    expect(await gitHubEditorPage.isSourceVisible()).toBe(false);
+    expect(await gitHubEditorPage.isEditorVisible()).toBe(true);
+
+    // check link to online editor from clipboard
+    await gitHubEditorPage.copyLinkToOnlineEditor();
+    const clipboadText = await tools.clipboard().getContent();
+    expect(clipboadText).toContain(EXPECTED_LINK);
+
+    // open full screen editor
+    const fullScreenPage = await gitHubEditorPage.fullScreen();
+    await fullScreenPage.getBpmnEditor();
+    await fullScreenPage.scrollToTop();
+    gitHubEditorPage = await fullScreenPage.exitFullscreen();
+
+    // open online editor
+    const onlineEditorPage = await gitHubEditorPage.openOnlineEditor();
+    await onlineEditorPage.getBpmnEditor();
+});
+
+afterEach(async () => {
+    await tools.finishTest("BPMN_basic_opepration_test");
+});
+
--- test/DmnTest.ts
+++ test/DmnTest.ts
@@ -4,79 +4,75 @@ import GitHubEditorPage from "./framework/github-editor/GitHubEditorPage";
 import GitHubListItem from "./framework/github-file-list/GitHubListItem";
 import GitHubListPage from "./framework/github-file-list/GitHubListPage";
 import Tools from "./utils/Tools";
-import { expect } from "chai";
 
-describe("DMN test", () => {
+let tools: Tools;
 
-    let tools: Tools;
+beforeEach(async () => {
+    tools = await Tools.init();
+});
 
-    beforeEach(async () => {
-        tools = await Tools.init();
-    });
+test("DMN basic operations test", async () => {
 
-    it("basic operations", async () => {
+    const WEB_PAGE = "https://github.com/kiegroup/kogito-examples/blob/stable/dmn-quarkus-example/src/main/resources";
+    const FILE_NAME = "Traffic Violation.dmn";
+    const EXPECTED_LINK = "kiegroup/kogito-examples/stable/dmn-quarkus-example/src/main/resources/Traffic%20Violation.dmn";
+    const DMN_NODES_NAMES = ["Driver", "Fine", "Decision Table", "Should the driver be suspended?", "Context", "Violation"];
 
-        const WEB_PAGE = "https://github.com/kiegroup/kogito-examples/blob/stable/dmn-quarkus-example/src/main/resources";
-        const FILE_NAME = "Traffic Violation.dmn";
-        const EXPECTED_LINK = "kiegroup/kogito-examples/stable/dmn-quarkus-example/src/main/resources/Traffic%20Violation.dmn";
-        const DMN_NODES_NAMES = ["Driver", "Fine", "Decision Table", "Should the driver be suspended?", "Context", "Violation"];
+    // check online editor link in the list
+    const gitHubListPage: GitHubListPage = await tools.openPage(GitHubListPage, WEB_PAGE);
+    const gitHubFile: GitHubListItem = await gitHubListPage.getFile(FILE_NAME);
+    const linkText = await gitHubFile.getLinkToOnlineEditor();
+    expect(linkText).toContain(EXPECTED_LINK);
 
-        // check online editor link in the list
-        const gitHubListPage: GitHubListPage = await tools.openPage(GitHubListPage, WEB_PAGE);
-        const gitHubFile: GitHubListItem = await gitHubListPage.getFile(FILE_NAME);
-        const linkText = await gitHubFile.getLinkToOnlineEditor();
-        expect(linkText).contains(EXPECTED_LINK);
+    // open DMN editor
+    let editorPage: GitHubEditorPage = await gitHubFile.open();
+    const dmnEditor: DmnEditor = await editorPage.getDmnEditor();
 
-        // open DMN editor
-        let editorPage: GitHubEditorPage = await gitHubFile.open();
-        const dmnEditor: DmnEditor = await editorPage.getDmnEditor();
+    // check dmn name
+    await dmnEditor.enter();
+    const sideBar = await dmnEditor.getSideBar();
+    const processProps = await sideBar.openProperties();
+    expect(await processProps.getDmnNameFromInput()).toEqual("Traffic Violation");
+    await sideBar.closeActiveSideBar();
 
-        // check dmn name
-        await dmnEditor.enter();
-        const sideBar = await dmnEditor.getSideBar();
-        const processProps = await sideBar.openProperties();
-        expect(await processProps.getDmnNameFromInput()).equals("Traffic Violation");
-        await sideBar.closeActiveSideBar();
+    // move annotation to canvas
+    const dmnPalette: DmnPallette = await dmnEditor.getDmnPalette();
+    await dmnPalette.dragAndDropAnnotationToCanvas();
 
-        // move annotation to canvas
-        const dmnPalette: DmnPallette = await dmnEditor.getDmnPalette();
-        await dmnPalette.dragAndDropAnnotationToCanvas();
+    // check DMN properties
+    const decisionNavigator = await sideBar.openDecisionNavigator();
+    expect(await (await decisionNavigator.getNodeNames()).sort()).toEqual(DMN_NODES_NAMES.concat("TextAnnotation-1").sort());
+    expect(await decisionNavigator.getDmnName()).toEqual("Traffic Violation");
+    await decisionNavigator.selectNode("Driver");
+    const nodeProps = await sideBar.openProperties();
+    expect(await nodeProps.getDmnNameFromInput()).toEqual("Driver");
 
-        // check DMN properties
-        const decisionNavigator = await sideBar.openDecisionNavigator();
-        expect(await decisionNavigator.getNodeNames()).to.have.members(DMN_NODES_NAMES.concat("TextAnnotation-1"));
-        expect(await decisionNavigator.getDmnName()).equals("Traffic Violation");
-        await decisionNavigator.selectNode("Driver");
-        const nodeProps = await sideBar.openProperties();
-        expect(await nodeProps.getDmnNameFromInput()).equals("Driver");
-        
-        await dmnEditor.leave();
-        
-        // open and check source editor
-        expect(await editorPage.isSourceVisible()).false;
-        expect(await editorPage.isEditorVisible()).true;
-        await editorPage.seeAsSource();
-        expect(await editorPage.isSourceVisible()).true;
-        expect(await editorPage.isEditorVisible()).false;
-        await editorPage.seeAsDiagram();
-        expect(await editorPage.isSourceVisible()).false;
-        expect(await editorPage.isEditorVisible()).true;
-        
-        // check link to online editor from clipboard
-        await editorPage.copyLinkToOnlineEditor();
-        const clipboadText = await tools.clipboard().getContent();
-        expect(clipboadText).contains(EXPECTED_LINK);
-        
-        const fullScreenPage = await editorPage.fullScreen();
-        await fullScreenPage.getDmnEditor();
-        await fullScreenPage.scrollToTop();
-        editorPage = await fullScreenPage.exitFullscreen();
-        
-        const onlineEditorPage = await editorPage.openOnlineEditor();
-        await onlineEditorPage.getDmnEditor();
-    });
+    await dmnEditor.leave();
 
-    afterEach(async () => {
-        await tools.finishTest("DMN_test");
-    });
+    // open and check source editor
+    expect(await editorPage.isSourceVisible()).toBe(false);
+    expect(await editorPage.isEditorVisible()).toBe(true);
+    await editorPage.seeAsSource();
+    expect(await editorPage.isSourceVisible()).toBe(true);
+    expect(await editorPage.isEditorVisible()).toBe(false);
+    await editorPage.seeAsDiagram();
+    expect(await editorPage.isSourceVisible()).toBe(false);
+    expect(await editorPage.isEditorVisible()).toBe(true);
+
+    // check link to online editor from clipboard
+    await editorPage.copyLinkToOnlineEditor();
+    const clipboadText = await tools.clipboard().getContent();
+    expect(clipboadText).toContain(EXPECTED_LINK);
+
+    const fullScreenPage = await editorPage.fullScreen();
+    await fullScreenPage.getDmnEditor();
+    await fullScreenPage.scrollToTop();
+    editorPage = await fullScreenPage.exitFullscreen();
+
+    const onlineEditorPage = await editorPage.openOnlineEditor();
+    await onlineEditorPage.getDmnEditor();
+});
+
+afterEach(async () => {
+    await tools.finishTest("DMN_basic_opepration_test");
 });
