bca5a69e3c00c9456165cd395f21bb8d14ca8e70
Tomas David <tdavid@redhat.com>
2020-07-09 14:26:09 +0200

    Add forgotten awaits

--- test/framework/editor/Properties.ts
+++ test/framework/editor/Properties.ts
@@ -6,7 +6,7 @@ export default class Properties extends PageFrament {
     private static readonly LABEL_LOCATOR = By.xpath("//h3[text()='Properties']");
 
     public async waitUntilLoaded(): Promise<void> {
-        this.tools.by(Properties.LABEL_LOCATOR).wait(1000).untilPresent();
+        await this.tools.by(Properties.LABEL_LOCATOR).wait(1000).untilPresent();
     }
 
     private getProperty(type: string, nameAttributeSuffix: string): By {
